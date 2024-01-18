63f3c82f9475ecb19f14d5c9cf4eac74949bf65a
awellsbiz <awellsbiz@gmail.com>
2024-01-17 19:32:38 -0800

    ts with variables

--- index.ts
+++ index.ts
@@ -2,7 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 // to get a notification when the promise is complete we chain on .then(with a callbback function)
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const { id, title, completed } = response.data as Todo;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(
+    `this is the id: ${id} | this is the title: ${title} | Has it been competed? ${completed}`
+  );
+};
